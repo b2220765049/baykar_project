339f92217f5d71627fb32b0b3acac3e2ad93c1d1
Emre <b2220765049@cs.hacettepe.edu.tr>
2025-09-18 12:15:47 +0300

    app_server file removed

--- app_server.py
+++
@@ -1,9 +0,0 @@
-"""
-app_server.py
-
-This module exposes the FastAPI `app` from `api.py` so that
-`uvicorn app_server:app` starts the single application that contains
-all routes (query, auth, status, etc.).
-"""
-
-from api import app
